a9ea6fadd5944eefb7d4a07dbee2c1eabae583a8
abwzerrialb <56445018+abwzerrialb@users.noreply.github.com>
2022-07-29 19:07:10 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-//import { createRoot } from 'react-dom/client';
+//import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import Header from './components/header';
 
@@ -13,4 +13,8 @@ const App = () => {
     )
 }
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
+
+// ReactDOM.render(<App/>,document.getElementById('root'))
